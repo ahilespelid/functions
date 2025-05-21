db22262f5d06c60a3133142f459f7e522e8bf183
Ahi Les <47417767+ahilespelid@users.noreply.github.com>
2025-05-21 16:48:07 +0300

    Update init.php

--- init.php
+++ init.php
@@ -154,6 +154,11 @@ return strtr($t, $converter);}
 if(!function_exists('mexplode')){function mexplode(array $delimiters, string $string){
     $chr = '::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::';
 return explode($chr, str_replace($delimiters, $chr, $string));}}
+///*/ Функция переводчик google///*/
+if(!function_exists('google_translate')){function google_translate($text, $from_lan = 'ru', $to_lan = 'en', $key = 'AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw'){
+    $obj = json_decode(file_get_contents('https://translation.googleapis.com/language/translate/v2?q='.urlencode($text).'&source='.$from_lan.'&target='.$to_lan .'&format=text&key='.$key), true);
+    $ret = trim($obj['data']['translations']['0']['translatedText']);
+return (empty($ret)) ? false : $ret;}}
 
 //------------------------------------------------------------------------------STRING-GENERATORS------------------------------------------------------------------------------------------------------------------------//  
 ///*/ Функция генерирует случайный префикс из ascii таблицы///*/
@@ -164,7 +169,6 @@ if(!function_exists('prefix')){function prefix(int $length = 5, bool $upcase = t
 return $ret;}}
 ///*/ Функция генерирует GUID из com_create_guid///*/
 if(!function_exists('GUID')){function GUID(){return strtoupper(com_create_guid());}}
-
 
 //------------------------------------------------------------------------------ANALOG-----------------------------------------------------------------------------------------------------------------------------------//  
 ///*/ Функция аналог str_starts_with из php 8 ///*/

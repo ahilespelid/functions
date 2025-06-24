4fec3e935f41e560c00e19955e025822e71bf61f
Ahi Les <47417767+ahilespelid@users.noreply.github.com>
2025-06-24 13:22:32 +0300

    Update init.php

--- init.php
+++ init.php
@@ -99,7 +99,7 @@ return $result;}}
 
 ///*/ функция рекурсивно чистит  многомерный массив по задданой структуре///*/
 if(!function_exists('array_keys_recursive_filter')){function array_keys_recursive_filter(array $array, array $keep_keys): array {
-    return array_map(fn($i) => is_array($i) ? filter_array_keys_recursive($i, $keep_keys) : $i, array_intersect_key($array, array_flip($keep_keys)));}
+    return array_map(fn($i) => is_array($i) ? filter_array_keys_recursive($i, $keep_keys) : $i, array_intersect_key($array, array_flip($keep_keys)));}}
 //------------------------------------------------------------------------------FILE-SYSTEM------------------------------------------------------------------------------------------------------------------------------//  
 ///*/ ahilespelid Метод возвращает путь до папки local/php_interface Bitrix при учёте что текущий файл лежит в local/php_interface///*/ 
 if(!function_exists('functions_path')){function functions_path(string $file=''){

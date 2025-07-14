dd9e22b0f89cde76a1fdf9bb5f29e255dc634c3a
Ahi Les <47417767+ahilespelid@users.noreply.github.com>
2025-07-14 17:06:52 +0300

    Update init.php

--- init.php
+++ init.php
@@ -120,6 +120,7 @@ return $ret;}}
 
 ///*/ ahilespelid Метод возвращает svg код из картинки svg ///*/ 
 if(!function_exists('svg')){function svg(string $file, string $default = null, array $attr = []): string {
+    if(empty($file)){return;}
     $svg = (is_svg($file = img_path($file))) ? $default ?? '<svg class="text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><line x1="12" y1="8" x2="12" y2="12"/><line x1="12" y1="16" x2="12.01" y2="16"/></svg>' : file_get_contents($file);
     $replace = function ($carry, $key) use ($attr) {$value = $attr[$key]; return (preg_match("/$key=\"[^\"]*\"/i", $carry)) ? preg_replace("/$key=\"[^\"]*\"/i", "$key=\"$value\"", $carry) : str_replace('<svg', "<svg $key=\"$value\"", $carry);};
 return array_reduce(array_keys($attr), $replace, $svg);}}
